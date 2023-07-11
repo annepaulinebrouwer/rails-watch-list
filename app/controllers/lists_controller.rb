3c538938d2d117c60bc435ef420b12fe68d76feb
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-11 18:30:43 +0200

    Added new and create action to the controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,23 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path notice: "You successfully created a movie list with the name: #{@list.name}"
+    else
+      flash[:alert] = 'Could not create the list  '
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
