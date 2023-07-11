d39445e9e976279f0a181becd472713893b81d40
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-11 16:33:31 +0200

    Using IDMB API for movies seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,19 @@
 
 puts "Cleaning database..."
 Movie.destroy_all
-
 puts "Creating movies..."
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-Movie.create(title: "No Man's Land",
-             overview: "Bosnia and Herzegovina during 1993 at the time of the heaviest fighting between the two warring sides. Two soldiers from opposing sides in the conflict, Nino and Ciki, become trapped in no man's land, whilst a third soldier becomes a living booby trap.",
-             poster_url: "https://www.imdb.com/title/tt0283509/mediaviewer/rm1167542272/",
-             rating: 7.9)
-Movie.create(title: "Victoria",
-            overview: "A young Spanish woman who has recently moved to Berlin finds her flirtation with a local guy turn potentially deadly as their night out with his friends reveals a dangerous secret.",
-            poster_url: "https://www.imdb.com/title/tt4226388/mediaviewer/rm2220682240/",
-            rating: 7.6)
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+html_content = URI.open(url).read
+json = JSON.parse(html_content)
+
+json["results"].each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    rating: movie["vote_average"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+  )
+end
+
 puts "Finished!"
