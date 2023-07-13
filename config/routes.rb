ee7c45d32b7413c123a073c5d1f9411e4aef9231
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-13 16:46:00 +0200

    Change lists_path of root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "lists#index"
-  resources :lists, only: [:index, :new, :create, :show] do
+  resources :lists, only: [:new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:create]
   end
